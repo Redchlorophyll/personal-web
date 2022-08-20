7347938ccabe28522d14da59c16dc93de02aecb3
dahs <hendra.dhonni.888@gmail.com>
2022-08-20 16:43:46 +0700

    feat: add eslint

--- front-end/pages/linky.tsx
+++ front-end/pages/linky.tsx
@@ -1,7 +1,10 @@
+import Button from '@/components/common/Button';
+
 export default function Linky() {
     return (
         <div>
-            <h1>Hello</h1>
+            <h1 className="card">Hello</h1>
+            <Button />
         </div>
     );
 }
