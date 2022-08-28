db2c07f38ffe486aada9f435fc3e3e90ddc98a86
dahs <hendra.dhonni.888@gmail.com>
2022-08-28 21:58:18 +0700

    test: add unit test for toggleMode

---
+++ front-end/tests/components/common/ToggleMode.test.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import {render, fireEvent, waitFor, screen} from '@testing-library/react'
+import '@testing-library/jest-dom'
+import ToggleMode from '@/components/common/ToggleMode/Index';
+import { Provider } from "react-redux";
+import { store } from "@/store";
+
+describe('components - common - ToggleMode', () => {
+  test('It should render properly', () => { 
+    render(<Provider store={store}><ToggleMode /></Provider>);
+
+    const toggleMode = screen.getByRole('checkbox', {
+      name: 'toggle-mode',
+    });
+
+    expect(toggleMode).toBeInTheDocument();
+  });
+
+  test('it should show dark mode icons when clicked', () => {
+    render(<Provider store={store}><ToggleMode /></Provider>);
+
+    const toggleMode = screen.getByRole('checkbox', {
+      name: 'toggle-mode',
+    });
+    
+    fireEvent.click(toggleMode);
+    
+    expect(screen.getByAltText('Dark mode icon')).toBeInTheDocument();
+  });
+
+  test('it should show light mode', () => {
+    render(<Provider store={store}><ToggleMode /></Provider>);
+
+    const toggleMode = screen.getByRole('checkbox', {
+      name: 'toggle-mode',
+    });
+    
+    fireEvent.click(toggleMode);
+    
+    expect(screen.getByAltText('Light mode icon')).toBeInTheDocument();
+  });
+});
