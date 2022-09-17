39c2b011885ca35a4bd059fa72cc02a6a443c813
dahs <hendra.dhonni.888@gmail.com>
2022-09-17 15:19:42 +0700

    feat: add dropdown functionality

--- front-end/components/common/Dropdown/Index.tsx
+++ front-end/components/common/Dropdown/Index.tsx
@@ -1,23 +1,71 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
+import { Fade } from "react-awesome-reveal";
 
 export default function Dropdown() {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const inputRef = useRef<HTMLHeadingElement>(null);
+
+  useEffect(() => {
+    const outsideClickHandler = ({ target }: MouseEvent) => {
+      const { current } = inputRef;
+
+      if (current && !current.contains(target as Node)) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", outsideClickHandler);
+
+    return () => {
+      document.removeEventListener("mousedown", outsideClickHandler);
+    };
+  });
   return (
-    <div className="w-full relative dark:text-black-900">
+    <div ref={inputRef} className="w-full relative dark:text-black-900">
       <input
         className="cursor-pointer bg-black-100 w-full p-[6px_17px_6px_13px] border-black-800 border-solid border-[0.5px] focus:outline-none focus:border-solid focus:border-[0.5px] focus:border-primary-800 rounded-lg dark:drop-shadow-[0px_1px_17px_#406fcb]"
         type="text"
         placeholder="Select data here..."
         readOnly
+        onClick={() => setIsOpen(!isOpen)}
       />
-      <div className="bg-dropdown-arrow w-5 h-3 bg-cover z-[1] absolute left-full top-[10px] -translate-x-[28px]" />
-      <ul className="w-full bg-black-300 absolute mt-2">
-        <li>Option 1</li>
-        <li>Option 2</li>
-        <li>Option 3</li>
-        <li>Option 4</li>
-        <li>Option 5</li>
-        <li>Option 6</li>
-      </ul>
+
+      <div className="bg-black-100 bg-dropdown-arrow w-5 h-3 bg-cover z-[1] absolute left-full top-[10px] -translate-x-[28px]" />
+      {isOpen ? (
+        <Fade>
+          <div className="w-full bg-black-100 absolute mt-2 p-[12px_5px_7px_5px] overflow-y-scroll max-h-32 scrollbar-thin scrollbar-thumb-black-500 scrollbar-track-dark-100 scrollbar-thumb-rounded-full scrollbar-track-rounded-full shadow-[0px_1px_4px_rgba(0,0,0,0.25)]">
+            <div className="list-inside leading-6 border-b-[1px] border-black-500 hover:bg-black-400">
+              Option test 1
+            </div>
+            <div className="list-inside leading-6 border-b-[1px] border-black-500 hover:bg-black-400">
+              Option test 1
+            </div>
+            <div className="list-inside leading-6 border-b-[1px] border-black-500 hover:bg-black-400">
+              Option test 1
+            </div>
+            <div className="list-inside leading-6 border-b-[1px] border-black-500 hover:bg-black-400">
+              Option test 1
+            </div>
+            <div className="list-inside leading-6 border-b-[1px] border-black-500 hover:bg-black-400">
+              Option test 1
+            </div>
+            <div className="list-inside leading-6 border-b-[1px] border-black-500 hover:bg-black-400">
+              Option test 1
+            </div>
+            <div className="list-inside leading-6 border-b-[1px] border-black-500 hover:bg-black-400">
+              Option test 1
+            </div>
+            <div className="list-inside leading-6 border-b-[1px] border-black-500 hover:bg-black-400">
+              Option test 1
+            </div>
+            <div className="list-inside leading-6 border-b-[1px] border-black-500 hover:bg-black-400">
+              Option test 1
+            </div>
+          </div>
+        </Fade>
+      ) : (
+        ""
+      )}
     </div>
   );
 }
--- front-end/tailwind.config.js
+++ front-end/tailwind.config.js
@@ -93,5 +93,11 @@ module.exports = {
       },
     },
   },
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("tailwind-scrollbar")({ nocompatible: true }),
+  ],
+  variants: {
+    scrollbar: ["rounded"],
+  },
 };
