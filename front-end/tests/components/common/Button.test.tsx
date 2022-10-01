402c47c249bb12ff77ed923f94510e346af6b9b2
ChandraRaditya <chandraariwiraditya@gmail.com>
2022-10-01 13:45:23 +0700

    chore: remove fireevent on button test

--- front-end/tests/components/common/Button.test.tsx
+++ front-end/tests/components/common/Button.test.tsx
@@ -1,4 +1,4 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import userEvent from "@testing-library/user-event";
 import Button from "@/components/common/Button/Index";
