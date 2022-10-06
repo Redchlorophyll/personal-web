1323a8f2ff0e22f27b9631799b7063869a80bfc7
ChandraRaditya <chandraariwiraditya@gmail.com>
2022-10-06 21:47:32 +0700

    refactor: change textarea props

--- front-end/components/common/Textarea/index.tsx
+++ front-end/components/common/Textarea/index.tsx
@@ -1,23 +1,28 @@
 import React from "react";
 
 type textareaProps = {
-  onChange: (event: string) => void;
-  value: string;
+  onChange?: (event: string) => void;
+  value?: string;
+  placeholder?: string;
 };
 
-const Textarea = ({ value, onChange }: textareaProps) => {
+const Textarea = ({ placeholder, value, onChange }: textareaProps) => {
+  const onChangeTextarea = (event: string) => {
+    if (onChange) {
+      onChange(event);
+    }
+  };
   return (
     <textarea
-      onChange={(event) => onChange(event.target.value)}
       value={value}
+      placeholder={placeholder || "Textarea"}
+      onChange={(event) => onChangeTextarea(event.target.value)}
       className={[
         "w-full h-[170px] outline-none border-[0.5px] border-solid",
         "border-[#464646] rounded-[7px] px-[13px] py-[8px]",
         "focus:dark:drop-shadow-[0px_1px_17px_#406FCB] dark:text-black-900",
       ].join(" ")}
-    >
-      {value}
-    </textarea>
+    ></textarea>
   );
 };
 
