f8d1147143658a976db06f672d668f2fb3ceb21b
ChandraRaditya <chandraariwiraditya@gmail.com>
2022-10-06 21:23:30 +0700

    feat: adding textarea

--- front-end/components/common/Textarea/index.tsx
+++ front-end/components/common/Textarea/index.tsx
@@ -1,14 +1,23 @@
 import React from "react";
 
-const Textarea = () => {
+type textareaProps = {
+  onChange: (event: string) => void;
+  value: string;
+};
+
+const Textarea = ({ value, onChange }: textareaProps) => {
   return (
     <textarea
+      onChange={(event) => onChange(event.target.value)}
+      value={value}
       className={[
         "w-full h-[170px] outline-none border-[0.5px] border-solid",
         "border-[#464646] rounded-[7px] px-[13px] py-[8px]",
         "focus:dark:drop-shadow-[0px_1px_17px_#406FCB] dark:text-black-900",
       ].join(" ")}
-    ></textarea>
+    >
+      {value}
+    </textarea>
   );
 };
 
