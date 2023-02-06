3fdde12d4f785b144e38d0d01001ed195e53c2f7
redchlorophyll <hendra.dhonni.888@gmail.com>
2023-02-06 20:32:40 +0700

    feat: disable button when login still in process

--- apps/web/pages/google-login.tsx
+++ apps/web/pages/google-login.tsx
@@ -1,12 +1,12 @@
-import React, { useEffect, useState } from 'react';
-import { useRouter } from 'next/router';
-import { UserAuth } from '@/context/Auth';
-import { Button } from 'ui';
-import Image from 'next/image';
+import React, { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import { UserAuth } from "@/context/Auth";
+import { Button } from "ui";
+import Image from "next/image";
 
 export default function GoogleLogin() {
   const { googleSignInWithRedirect, user, logout } = UserAuth();
-  const [ isLoggedIn, setIsLoggedIn ] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const router = useRouter();
 
   const onClickGoogleSignIn = async () => {
@@ -18,63 +18,81 @@ export default function GoogleLogin() {
   };
 
   useEffect(() => {
-    if (localStorage.getItem('isLogin') === 'true') setIsLoggedIn(true);
+    if (localStorage.getItem("isLogin") === "true") setIsLoggedIn(true);
     else setIsLoggedIn(false);
 
     // watch changes in localStorage
-    window.addEventListener('storage', () => {
-      if (localStorage.getItem('isLogin') === 'true') setIsLoggedIn(true);
+    window.addEventListener("storage", () => {
+      if (localStorage.getItem("isLogin") === "true") setIsLoggedIn(true);
       else setIsLoggedIn(false);
     });
   }, []);
 
   return (
-    <div className='w-full flex justify-center text-center'>
-      <div className='m-[180px]'>
-        <div className='pb-5'>
-          <h1 className='mb-2'>Google Sign Up</h1>
-          {
-            isLoggedIn && Object.keys(user || {}).length === 0 ?
-            (
-              <div className='flex justify-center'>
-                <div className="bg-[url('../img/icons/ic_loading.svg')] w-[40px] h-[40px] bg-contain animate-spin"/>
-              </div>
-            ) : ''
-          }
-          {
-            Object.keys(user || {}).length === 0 ?
-            '' : (
-              <>
-                <div className='font-semibold mb-2'>Logged in as,</div>
-                <div className='border-2 border-black-200 py-2 pl-3 pr-5 rounded-md flex bor'>
-                  <div className='w-[54px] h-[54px] rounded-full overflow-hidden relative mr-4'>
-                    <Image
-                      fill
-                      src={user?.photoURL}
-                      alt="user profile image" />
+    <div className="w-full flex justify-center text-center">
+      <div className="m-[180px]">
+        <div className="pb-5">
+          <h1 className="mb-2">Google Sign Up</h1>
+          {isLoggedIn && Object.keys(user || {}).length === 0 ? (
+            <div className="flex justify-center">
+              <div className="bg-[url('../img/icons/ic_loading.svg')] w-[40px] h-[40px] bg-contain animate-spin" />
+            </div>
+          ) : (
+            ""
+          )}
+          {Object.keys(user || {}).length === 0 ? (
+            ""
+          ) : (
+            <>
+              <div className="font-semibold mb-2">Logged in as,</div>
+              <div className="border-2 border-black-200 py-2 pl-3 pr-5 rounded-md flex bor">
+                <div className="w-[54px] h-[54px] rounded-full overflow-hidden relative mr-4">
+                  <Image fill src={user?.photoURL} alt="user profile image" />
+                </div>
+                <div>
+                  <div className="flex justify-start">
+                    <span className="font-bold text-left">
+                      {user?.displayName}
+                    </span>
                   </div>
-                  <div>
-                    <div className='flex justify-start'>
-                      <span className='font-bold text-left'>{user?.displayName}</span>
-                    </div>
-                    <div className='flex justify-start'>
-                      <span className='text-black-700 text-left'>{user?.email}</span>
-                    </div>
+                  <div className="flex justify-start">
+                    <span className="text-black-700 text-left">
+                      {user?.email}
+                    </span>
                   </div>
                 </div>
-              </>
-            )
-          }
+              </div>
+            </>
+          )}
         </div>
 
-        <div className='pb-2'>
-          {
-            Object.keys(user || {}).length === 0 ?
-            isLoggedIn? '' : (<Button onClick={onClickGoogleSignIn}>Google Sign In</Button>) :
-            (<Button variant='muted' type='translucent' onClick={logout}>Logout</Button>)
-          }
+        <div className="pb-2">
+          {Object.keys(user || {}).length === 0 ? (
+            isLoggedIn ? (
+              ""
+            ) : (
+              <Button onClick={onClickGoogleSignIn}>Google Sign In</Button>
+            )
+          ) : (
+            <Button variant="muted" type="translucent" onClick={logout}>
+              Logout
+            </Button>
+          )}
         </div>
-        <Button onClick={() => router.push('/linky')}>Go back to Linky Page</Button>
+        <Button
+          variant={
+            isLoggedIn && Object.keys(user || {}).length === 0
+              ? "muted"
+              : "primary"
+          }
+          onClick={() =>
+            isLoggedIn && Object.keys(user || {}).length === 0
+              ? ""
+              : router.push("/linky")
+          }
+        >
+          Go back to Linky Page
+        </Button>
       </div>
     </div>
   );
