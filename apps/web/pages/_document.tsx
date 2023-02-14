e3309004a812c8006c5604470d0e62fb49217dc5
redchlorophyll <hendra.dhonni.888@gmail.com>
2023-02-14 21:36:15 +0700

    feat: replace apple-touch-icon-precomposed to apple-touch-icon

--- apps/web/pages/_document.tsx
+++ apps/web/pages/_document.tsx
@@ -11,10 +11,7 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <link rel="shortcut icon" href="/favicon/favicon.ico" />
-          <link
-            rel="apple-touch-icon-precomposed"
-            href="/favicon/apple-touch-icon.png"
-          />
+          <link rel="apple-touch-icon" href="/favicon/apple-touch-icon.png" />
           <link rel="icon" href="favicon-32x32.png" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
         </Head>
